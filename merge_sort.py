2634bbd10db25af59c601a9a1e54c0fb57000175
이건희 <79892675+LOOPARAM@users.noreply.github.com>
2024-08-26 23:46:14 +0900

    merge_sort

--- merge_sort.py
+++ merge_sort.py
@@ -11,44 +11,54 @@ def random_data(len,start,end):
         data.append(random.randint(start,end))
     return data
 
-def merge_sort(data):
+def split_data(data):
     sub_data1 = []
     sub_data2 = []
-    result = []
 
-    if(len(data) == 2):
-        if(data[0]>data[1]):
-            data[0],data[1] = data[1],data[0]
-        return data
+    if(len(data)%2 == 0):
+        sub_data1 = (data[0:len(data)//2])
+        sub_data2 = (data[(len(data)//2):len(data)])
     else:
-        if(len(data)%2 == 0):
-            sub_data1 = merge_sort(data[0:len(data)//2])
-            sub_data2 = merge_sort(data[(len(data)//2):len(data)])
-            print(sub_data1,sub_data2)
+        sub_data1 = (data[0:(len(data)+1)//2])
+        sub_data2 = (data[((len(data)+1)//2):len(data)])
+    
+    return sub_data1,sub_data2
 
-            pointer = 0
-            pointer1 = 0
-            while(len(result)<=len(sub_data1+sub_data2)):
-                print("result",result,pointer,pointer1)
-                if(pointer1 == len(sub_data1)):
-                    ???
-                if(sub_data1[pointer] > sub_data2[pointer1]):
-                    result.append(sub_data2[pointer1])
-                    pointer1+=1
-                else:
-                    result.append(sub_data1[pointer])
-                    pointer+=1
-                time.sleep(1)
-                    
+def sort(data1,data2):
+    result = []
+    p1 = 0
+    p2 = 0
+    for i in range(0,(len(data1)*2)):
+        if(p1==len(data1)):
+            for j in range(p2,len(data2)):
+                result.append(data2[j])
+            return result
+        if(p2==len(data2)):
+            for j in range(p1,len(data1)):
+                result.append(data1[j])
+            return result
+
+        if(data1[p1] < data2[p2]):
+            result.append(data1[p1])
+            p1 += 1
         else:
-            sub_data1 = merge_sort(data[0:(len(data)+1)//2])
-            sub_data2 = merge_sort(data[((len(data)+1)//2):len(data)])
+            result.append(data2[p2])
+            p2 += 1
+        # print(result,p1,p2)
+    return result
+
+def merge_sort(data):
+
+    if(len(data) == 1):
+        return data
+    sub_data1 = merge_sort(split_data(data)[0])
+    sub_data2 = merge_sort(split_data(data)[1])
+    # print(sub_data1,sub_data2)
+    result = sort(sub_data1,sub_data2)
 
     return result
 
-        
-
-# data = random_data(10,1,30)
-data = [43,38,82,19,57,26,64,71]
-print(data)
-print(merge_sort(data))
+data = random_data(10,1,30)
+# data = [43,38,82,19,57,26,64,71] #
+print(" start data : ",data)
+print("result data : ",merge_sort(data))
